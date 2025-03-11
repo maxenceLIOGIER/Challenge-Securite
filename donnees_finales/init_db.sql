6cfe904b23421cda1bf380071176323ce413146d
joel-sll <joel.sollari@gmail.com>
2025-03-11 09:57:28 +0100

    correctifs postgres

--- donnees_finales/init_db.sql
+++ donnees_finales/init_db.sql
@@ -1,19 +1,28 @@
 CREATE TABLE logs (
     id SERIAL PRIMARY KEY,
     date TIMESTAMP,
+    action TEXT,
+    rule TEXT,
+    interface_in TEXT,
     ipsrc TEXT,
     ipdst TEXT,
     proto TEXT,
     portsrc FLOAT,
     portdst FLOAT,
-    regle FLOAT,
-    action TEXT,
-    interface_in TEXT,
-    interface_out TEXT,
-    divers FLOAT
+    year INT,
+    month INT,
+    day INT,
+    time TIME,
+    portsrc_range TEXT,
+    portdst_range TEXT,
+    hour INT,
+    ipsrc_class TEXT,
+    ipdst_class TEXT,
+    isprivatesrc TEXT,
+    isprivatedst TEXT
 );
 
-COPY logs(date, ipsrc, ipdst, proto, portsrc, portdst, regle, action, interface_in, interface_out, divers)
-FROM 'logs_processed.csv'
+COPY logs(date, action, rule, interface_in, ipsrc, ipdst, proto, portsrc, portdst, year, month, day, time, portsrc_range, portdst_range, hour, ipsrc_class, ipdst_class, isprivatesrc, isprivatedst)
+FROM '/data/logs_processed.csv'
 DELIMITER ','
 CSV HEADER;
